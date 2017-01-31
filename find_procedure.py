8547f964cb724767c4bb7b9559e689b024aa0415
maxim1982 <maxim_7@mail.ru>
2017-01-31 23:26:57 +0300

    Add files via upload
    Задание к занятию «Работа с папками, путями»

--- find_procedure.py
+++ find_procedure.py
@@ -1,51 +1,33 @@
-
-# заготовка для домашней работы
-# прочитайте про glob.glob
-# https://docs.python.org/3/library/glob.html
-
-# Задание
-# мне нужно отыскать файл среди десятков других
-# я знаю некоторые части этого файла (на память или из другого источника)
-# я ищу только среди .sql файлов
-# 1. программа ожидает строку, которую будет искать (input())
-# после того, как строка введена, программа ищет её во всех файлах
-# выводит список найденных файлов построчно
-# выводит количество найденных файлов
-# 2. снова ожидает ввод
-# поиск происходит только среди найденных на этапе 1
-# 3. снова ожидает ввод
-# ...
-# Выход из программы программировать не нужно.
-# Достаточно принудительно остановить, для этого можете нажать Ctrl + C
-
-# Пример на настоящих данных
-
-# python3 find_procedure.py
-# Введите строку: INSERT
-# ... большой список файлов ...
-# Всего: 301
-# Введите строку: APPLICATION_SETUP
-# ... большой список файлов ...
-# Всего: 26
-# Введите строку: A400M
-# ... большой список файлов ...
-# Всего: 17
-# Введите строку: 0.0
-# Migrations/000_PSE_Application_setup.sql
-# Migrations/100_1-32_PSE_Application_setup.sql
-# Всего: 2
-# Введите строку: 2.0
-# Migrations/000_PSE_Application_setup.sql
-# Всего: 1
-
-# не забываем организовывать собственный код в функции
-# на зачёт с отличием, использовать папку 'Advanced Migrations'
-
 import glob
 import os.path
 
-migrations = 'Migrations'
+#migrations = 'Migrations'
+migrations = 'Advanced Migrations'
 
+#  бесконечный цикл
+exit_while = False
+
+files = ''
 files = glob.glob(os.path.join(migrations, "*.sql"))
-for file in files:
-	print(file)
+
+while exit_while is False:
+    files_new = list()
+    find_file = 0
+    print('Введите строку ( или q - выход.): ')
+    text_find = input()
+
+    if text_find == 'q':
+        exit_while = True
+    else:
+        for file in files:
+            with open(file) as fopen:
+                text = fopen.read()
+                if text_find in text:
+                    print(file)
+                    find_file += 1
+                    files_new.append(file)
+
+    print('Кол-во :', find_file)
+    files = files_new
+
+
